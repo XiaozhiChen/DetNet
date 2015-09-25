bc747f02446a152e74f0f144afd52d154e5638d6
XiaozhiChen <cxz.thu@gmail.com>
2015-09-25 19:13:55 +0800

    scripts for local uses

--- data/scripts/fetch_imagenet_models.sh
+++ data/scripts/fetch_imagenet_models.sh
@@ -1,34 +1,38 @@
 #!/bin/bash
-
+# Soft link for local use
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../" && pwd )"
-cd $DIR
+ln -s /u/xiaozhi/projects/coco_detection/code/DetNet/data/imagenet_models $DIR
+echo "imagenet_models are places in $DIR"
 
-FILE=imagenet_models.tgz
-URL=http://www.cs.berkeley.edu/~rbg/fast-rcnn-data/$FILE
-CHECKSUM=8b1d4b9da0593fc70ef403284f810adc
-
-if [ -f $FILE ]; then
-  echo "File already exists. Checking md5..."
-  os=`uname -s`
-  if [ "$os" = "Linux" ]; then
-    checksum=`md5sum $FILE | awk '{ print $1 }'`
-  elif [ "$os" = "Darwin" ]; then
-    checksum=`cat $FILE | md5`
-  fi
-  if [ "$checksum" = "$CHECKSUM" ]; then
-    echo "Checksum is correct. No need to download."
-    exit 0
-  else
-    echo "Checksum is incorrect. Need to download again."
-  fi
-fi
-
-echo "Downloading pretrained ImageNet models (1G)..."
-
-wget $URL -O $FILE
-
-echo "Unzipping..."
-
-tar zxvf $FILE
-
-echo "Done. Please run this command again to verify that checksum = $CHECKSUM."
+#DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../" && pwd )"
+#cd $DIR
+#
+#FILE=imagenet_models.tgz
+#URL=http://www.cs.berkeley.edu/~rbg/fast-rcnn-data/$FILE
+#CHECKSUM=8b1d4b9da0593fc70ef403284f810adc
+#
+#if [ -f $FILE ]; then
+#  echo "File already exists. Checking md5..."
+#  os=`uname -s`
+#  if [ "$os" = "Linux" ]; then
+#    checksum=`md5sum $FILE | awk '{ print $1 }'`
+#  elif [ "$os" = "Darwin" ]; then
+#    checksum=`cat $FILE | md5`
+#  fi
+#  if [ "$checksum" = "$CHECKSUM" ]; then
+#    echo "Checksum is correct. No need to download."
+#    exit 0
+#  else
+#    echo "Checksum is incorrect. Need to download again."
+#  fi
+#fi
+#
+#echo "Downloading pretrained ImageNet models (1G)..."
+#
+#wget $URL -O $FILE
+#
+#echo "Unzipping..."
+#
+#tar zxvf $FILE
+#
+#echo "Done. Please run this command again to verify that checksum = $CHECKSUM."
