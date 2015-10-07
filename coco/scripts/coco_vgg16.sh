dc80ad578c3ca7054bc1653559ce6f5808b84f75
XiaozhiChen <cxz.thu@gmail.com>
2015-10-07 21:29:59 +0800

    context rcnn scripts

--- coco/scripts/coco_vgg16.sh
+++ coco/scripts/coco_vgg16.sh
@@ -5,20 +5,20 @@ set -e
 
 export PYTHONUNBUFFERED="True"
 
-LOG="experiments/logs/coco_baseline_vgg16_crowd.txt.`date +'%Y-%m-%d_%H-%M-%S'`"
+LOG="experiments/logs/coco_$2_vgg16_crowd.txt.`date +'%Y-%m-%d_%H-%M-%S'`"
 exec &> >(tee -a "$LOG")
 echo Logging output to "$LOG"
 
 time ./tools/train_net.py --gpu $1 \
-  --solver models/VGG16/coco/solver.prototxt \
+  --solver coco/models/VGG16/$2/solver.prototxt \
   --weights data/imagenet_models/VGG16.v2.caffemodel \
-  --cfg experiments/cfgs/coco.yml \
+  --cfg coco/cfgs/coco.yml \
   --imdb coco_2014_train \
   --iters 240000
 
 time ./tools/test_net.py --gpu $1 \
-  --def models/VGG16/coco/test.prototxt \
-  --net output/coco_baseline/coco_2014_train/vgg16_fast_rcnn_iter_240000.caffemodel \
-  --cfg experiments/cfgs/coco.yml \
+  --def coco/models/VGG16/$2/test.prototxt \
+  --net output/coco_$2/coco_2014_train/vgg16_fast_rcnn_iter_240000.caffemodel \
+  --cfg coco/cfgs/coco.yml \
   --imdb coco_2014_minival \
   --num_dets 100
